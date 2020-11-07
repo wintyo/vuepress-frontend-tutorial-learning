e125d0b20679fa21e29347561e9fc405bb897980
wintyo <wintyo1024@gmail.com>
2020-11-07 17:21:20 +0900

    blogプラグインを入れてみた

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,14 +1,40 @@
 module.exports = {
-  title: 'フロントエンドチュートリアル学習サイト',
-  description: 'チュートリアル形式で学習できるサイト',
+  title: 'テストページ',
+  // title: 'フロントエンドチュートリアル学習サイト',
+  description: 'テスト',
+  // description: 'チュートリアル形式で学習できるサイト',
   dest: 'dist/',
   port: 8000,
   themeConfig: {
+    nav: [
+      { text: 'Tags', link: '/tag/' },
+    ],
     sidebar: 'auto',
+    sidebarDepth: 2,
   },
   // マークダウン機能の設定
   markdown: {
     lineNumbers: true,
     linkify: true,
   },
+  plugins: [
+    ['@vuepress/blog', {
+      directories: [
+        {
+          id: 'post',
+          dirname: 'posts',
+          itemPermalink: '/:year/:slug',
+        },
+      ],
+      frontmatters: [
+        {
+          id: 'tag',
+          keys: ['tags'],
+          path: '/tag/',
+          layout: 'Tag',
+          scopeLayout: 'Tag',
+        },
+      ],
+    }],
+  ],
 };
